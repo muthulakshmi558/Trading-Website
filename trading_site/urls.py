bcfbd31781381b644dc1c3cfef5002334db1f673
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-09-06 04:53:16 +0530

    Update urls.py

--- trading_site/urls.py
+++ trading_site/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for trading_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,5 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tradingapp.urls')),  # include app urls
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('tradingapp.urls')),  # Include app URLs
+]
+
+# Serve media files in development only
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Note: In production (DEBUG=False), static files and media files
+# should be served via the web server or Render's static files configuration.
